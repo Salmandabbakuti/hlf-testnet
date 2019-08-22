9266d2a467094540e101bb54ecfdd873c1b43ccb
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-22 16:57:53 +0530

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -4,7 +4,10 @@ mkdir channel-artifacts
 export PATH=${PWD}/bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=$PWD
 echo 'Generating Certificates...'
-../bin/configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
-../bin/configtxgen -profile ThreeOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
+
+cryptogen generate --config=crypto-config.yaml
+
+configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
+configtxgen -profile ThreeOrgsChannel -outputCreateChannelTx ./channel-artifacts/channel.tx -channelID mychannel
 
 echo 'All Done..'
