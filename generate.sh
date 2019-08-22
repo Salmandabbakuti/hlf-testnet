39d6a8cf7008c8276808616b8119e31a05dbbed3
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-22 16:55:09 +0530

    Update generate.sh

--- generate.sh
+++ generate.sh
@@ -1,6 +1,7 @@
 rm -rf crypto-config
 rm -rf channel-artifacts
 mkdir channel-artifacts
+export PATH=${PWD}/bin:${PWD}:$PATH
 export FABRIC_CFG_PATH=$PWD
 echo 'Generating Certificates...'
 ../bin/configtxgen -profile ThreeOrgsOrdererGenesis -outputBlock ./channel-artifacts/genesis.block
