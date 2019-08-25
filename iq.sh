02272371b76029e8fbe85b4683b965ce0f63b5b9
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-25 12:05:47 +0530

    Update iq.sh

--- iq.sh
+++ iq.sh
@@ -1,6 +1,6 @@
 echo 'Invoking Chaincode From Org1 Peer'
 
-sudo docker exec -it cli peer chaincode invoke -o orderer0.example.com:7050 -n mycc -c '{"Args":["set", "a", "20"]}' -C mychannel
+sudo docker exec -it cli peer chaincode invoke -o orderer.example.com:7050 -n mycc -c '{"Args":["set", "a", "20"]}' -C mychannel
 
 sleep 5
 
@@ -10,7 +10,7 @@ sudo docker exec -it cli peer chaincode query -n mycc -c '{"Args":["query","a"]}
 
 echo 'Invoking Chaincode From Org2 Peer'
 
-sudo docker exec -it cli2 peer chaincode invoke -o orderer0.example.com:7050 -n mycc -c '{"Args":["set", "a", "40"]}' -C mychannel
+sudo docker exec -it cli2 peer chaincode invoke -o orderer.example.com:7050 -n mycc -c '{"Args":["set", "a", "40"]}' -C mychannel
 
 sleep 5
 
@@ -20,7 +20,7 @@ sudo docker exec -it cli2 peer chaincode query -n mycc -c '{"Args":["query","a"]
 
 echo 'Invoking Chaincode From Org3 Peer'
 
-sudo docker exec -it cli3 peer chaincode invoke -o orderer0.example.com:7050 -n mycc -c '{"Args":["set", "a", "60"]}' -C mychannel
+sudo docker exec -it cli3 peer chaincode invoke -o orderer.example.com:7050 -n mycc -c '{"Args":["set", "a", "60"]}' -C mychannel
 
 sleep 5
 
@@ -28,4 +28,4 @@ echo 'Querying For Result on Org3 Peer'
 
 sudo docker exec -it cli3 peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
 
-echo 'All Done.. You're Awesome.. have a great day... bye!!'
+echo 'All Done.. You're Awesome.. have a great day... bye..'
