65cfdbbbeffe0b92aeb6b876be77696471745fc8
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-08-25 10:44:20 +0530

    Create iq.sh

---
+++ iq.sh
@@ -0,0 +1,31 @@
+echo 'Invoking Chaincode From Org1 Peer'
+
+sudo docker exec -it cli peer chaincode invoke -o orderer0.example.com:7050 -n mycc -c '{"Args":["set", "a", "20"]}' -C mychannel
+
+sleep 5
+
+echo 'Querying For Result on Org1 Peer'
+
+sudo docker exec -it cli peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
+
+echo 'Invoking Chaincode From Org2 Peer'
+
+sudo docker exec -it cli2 peer chaincode invoke -o orderer0.example.com:7050 -n mycc -c '{"Args":["set", "a", "40"]}' -C mychannel
+
+sleep 5
+
+echo 'Querying For Result on Org2 Peer'
+
+sudo docker exec -it cli2 peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
+
+echo 'Invoking Chaincode From Org3 Peer'
+
+sudo docker exec -it cli3 peer chaincode invoke -o orderer0.example.com:7050 -n mycc -c '{"Args":["set", "a", "60"]}' -C mychannel
+
+sleep 5
+
+echo 'Querying For Result on Org3 Peer'
+
+sudo docker exec -it cli3 peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
+
+echo 'All Done.. You're Awesome.. have a great day... bye!!'
