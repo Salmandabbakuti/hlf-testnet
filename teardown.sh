dd037c6495a8fecde17779248691a969af2314bf
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-03 06:50:23 +0530

    Update teardown.sh

--- teardown.sh
+++ teardown.sh
@@ -1,10 +1,17 @@
  
 echo 'Clearing Network..'
 
+echo 'Clearing  docker containers..'
 
-sudo docker-compose -f docker-compose-cli.yaml down
+#STOP AND DELETE THE DOCKER CONTAINERS
+docker ps -aq | xargs -n 1 docker stop
+docker ps -aq | xargs -n 1 docker rm -v
 
-sudo docker rm $(docker ps -a -q)
+# DELETE THE OLD DOCKER VOLUMES
+docker volume prune
+
+# DELETE OLD DOCKER NETWORKS (OPTIONAL: seems to restart fine without)
+docker network prune
 
 echo 'All Done ..Bye..'
 exit 1
