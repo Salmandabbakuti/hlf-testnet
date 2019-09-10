6803e24dd458ed277b1d931cdf1dd660e2d2bac3
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-10 10:50:44 +0530

    Update invoke.js

--- client/invoke.js
+++ client/invoke.js
@@ -29,7 +29,7 @@ async function main() {
         const network = await gateway.getNetwork('mychannel');
 
         // Get the contract from the network.
-        const contract = network.getContract('mycc');
+        const contract = network.getContract('ecom');
 
         await contract.submitTransaction('initLedger');
         console.log('Transaction has been submitted');
