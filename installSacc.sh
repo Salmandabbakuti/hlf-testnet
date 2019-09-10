2dc8025ce8c174f89ebff5a0a1c1fd4c7ca6d456
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-10 14:31:51 +0530

    Simple Asset chaincode

---
+++ installSacc.sh
@@ -0,0 +1,15 @@
+echo 'Installing Chaincode  On All Peers..'
+
+sudo docker exec -it cli3 peer chaincode install -n mycc -p github.com/chaincode -v v0
+
+sudo docker exec -it cli2 peer chaincode install -n mycc -p github.com/chaincode -v v0
+
+sudo docker exec -it cli peer chaincode install -n mycc -p github.com/chaincode -v v0
+
+echo 'Instantiating Chaincode on Channel..'
+
+sudo docker exec -it cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc github.com/chaincode -v v0 -c '{"Args": ["a", "100"]}'
+
+echo 'All Done.. Start Invoking and Querying with *iq.sh* ..'
+
+exit 1
