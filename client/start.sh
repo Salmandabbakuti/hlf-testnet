130f66e3a2fbce9810b392b5180de07fe80289bd
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-12 20:12:47 +0530

    Update start.sh

--- client/start.sh
+++ client/start.sh
@@ -1,15 +1,5 @@
 cd ..
-echo 'Cloning test-net..'
-
-git clone https://github.com/Salmandabbakuti/hlf-testnet.git
-
-echo 'Mounting Chaincode..'
-cd hlf-testnet
-rm -rf chaincode
-cd ..
-cp -r chaincode hlf-testnet
 echo 'Initiating  Network Creation..'
-cd hlf-testnet
 echo 'Cleaning Containers..'
 sudo docker-compose -f docker-compose-cli.yaml down
 sudo docker volume prune
@@ -20,8 +10,8 @@ chmod a+x installCc.sh
 ./generate.sh
 ./start.sh
 ./installCc.sh
-cd ..
 cd client
+echo 'Preparing Client Application..'
 chmod a+x prereqs.sh
 ./prereqs.sh
 
