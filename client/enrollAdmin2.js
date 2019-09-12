84c7a865c225f947d0c493fe29eed5c4247731de
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-12 20:23:31 +0530

    Update enrollAdmin2.js

--- client/enrollAdmin2.js
+++ client/enrollAdmin2.js
@@ -5,7 +5,7 @@ const { FileSystemWallet, X509WalletMixin } = require('fabric-network');
 const fs = require('fs');
 const path = require('path');
 
-const ccpPath = path.resolve(__dirname, '..', 'hlf-testnet', 'connection-org2.json');
+const ccpPath = path.resolve(__dirname, '..', 'connection-org2.json');
 const ccpJSON = fs.readFileSync(ccpPath, 'utf8');
 const ccp = JSON.parse(ccpJSON);
 
