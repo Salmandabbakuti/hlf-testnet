38ac3ea310bd4a62304c3207c2b1370cf7c2d10b
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-12 20:22:13 +0530

    Update invoke.js

--- client/invoke.js
+++ client/invoke.js
@@ -3,7 +3,7 @@
 const { FileSystemWallet, Gateway } = require('fabric-network');
 const path = require('path');
 
-const ccpPath = path.resolve(__dirname, '..', 'hlf-testnet', 'connection-org1.json');
+const ccpPath = path.resolve(__dirname, '..', 'connection-org1.json');
 
 async function main() {
     try {
