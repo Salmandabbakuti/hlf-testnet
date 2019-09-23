c15ecf4ba86c48808f9c9cf2a477c13ae597badf
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-23 13:30:10 +0530

    Update installSacc.sh

--- installSacc.sh
+++ installSacc.sh
@@ -6,9 +6,13 @@ sudo docker exec -it cli2 peer chaincode install -n mycc -p github.com/chaincode
 
 sudo docker exec -it cli peer chaincode install -n mycc -p github.com/chaincode -v v0
 
-echo 'Instantiating Chaincode on Channel..'
+echo 'Instantiating Chaincode on mychannel..'
 
 sudo docker exec -it cli peer chaincode instantiate -o orderer.example.com:7050 -C mychannel -n mycc github.com/chaincode -v v0 -c '{"Args": ["a", "100"]}'
+
+echo 'Instantiating Chaincode on channel12..'
+
+sudo docker exec -it cli peer chaincode instantiate -o orderer.example.com:7050 -C channel12 -n mycc github.com/chaincode -v v0 -c '{"Args": ["a", "100"]}'
 
 echo 'All Done.. Start Invoking and Querying with *iq.sh* ..'
 
