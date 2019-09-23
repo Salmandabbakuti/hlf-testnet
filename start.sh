db014c876cfc5914c06378afeb34e99ebf880654
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-23 13:22:07 +0530

    Channel12 configuration

--- start.sh
+++ start.sh
@@ -26,6 +26,20 @@ echo 'Org3 Peer joining channel...'
 sudo docker exec -it cli3 peer channel join -b mychannel.block
 
 
-echo 'All Peers Joined channel..'
+echo 'All Peers Joined mychannel..'
 
+echo 'creating channel12..'
+sudo docker exec -it cli peer channel create -o orderer.example.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -c channel12 -f ./channel-artifacts/channel12.tx
+sudo docker exec -it cli peer channel join -b channel12.block
+
+echo 'exporting channel12.block to org2 peer..'
+
+sudo docker cp cli:/opt/gopath/src/github.com/hyperledger/fabric/peer/channel12.block .
+
+sudo docker cp channel12.block cli2:/opt/gopath/src/github.com/hyperledger/fabric/peer/channel12.block
+
+sudo docker exec -it cli2 peer channel join -b channel12.block
+rm channel12.block
+
+echo 'Peer2 joined channel12 successfully..'
 exit 1
