25cfbcd2d6b10f214c13878faca4660802e1287f
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-09-30 07:03:04 +0530

    Create restore.sh

---
+++ restore.sh
@@ -0,0 +1,25 @@
+set -ev
+
+#bringing network down and clearing volumes
+
+sudo docker-compose -f docker-compose.yml down
+
+sudo docker volume prune
+
+sudo docker network prune
+
+#Bringing network Up with Previous Backup
+
+sudo docker-compose -f restore-network.yml up -d
+#All done...
+sleep 20
+
+#querying Data
+
+sudo docker exec -it cli peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
+sudo docker exec -it cli2 peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
+sudo docker exec -it cli3 peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
+
+
+
+exit 1
