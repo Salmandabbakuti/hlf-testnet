75bf6e4427b4582a5e0bdb34663f71be7f72501b
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-11-14 11:20:08 +0530

    Update installEcomCc.sh

--- installEcomCc.sh
+++ installEcomCc.sh
@@ -1,12 +1,12 @@
-echo 'Installing Chaincode on Org1 Peer..'
+echo 'Installing Chaincode on Org1 Peer0..'
 
 sudo docker exec -it cli peer chaincode install -n ecom -p /opt/gopath/src/github.com/chaincode/ecom -v v0 -l "node" #installing chaincode on peer 0 via cli
 
-echo 'Installing Chaincode on Org2 Peer..'
+echo 'Installing Chaincode on Org2 Peer1..'
 
 sudo docker exec -it cli2 peer chaincode install -n ecom -p /opt/gopath/src/github.com/chaincode/ecom -v v0 -l "node" #installing chaincode on peer 0 via cli
 
-echo 'Installing Chaincode on Org3 Peer..'
+echo 'Installing Chaincode on Peer2..'
 
 sudo docker exec -it cli3 peer chaincode install -n ecom -p /opt/gopath/src/github.com/chaincode/ecom -v v0 -l "node" #installing chaincode on peer 0 via cli
 
