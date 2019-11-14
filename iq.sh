27ba2ea9a4fefc49d2d6f7902a2c90aef966e9f8
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-11-14 11:23:05 +0530

    Update iq.sh

--- iq.sh
+++ iq.sh
@@ -1,38 +1,27 @@
-echo 'Invoking Chaincode From Org1 Peer'
+echo 'Invoking Chaincode From Peer0'
 
 sudo docker exec -it cli peer chaincode invoke -o orderer.example.com:7050 -n mycc -c '{"Args":["set", "a", "20"]}' -C mychannel
-
-sudo docker exec -it cli peer chaincode invoke -o orderer.example.com:7050 -n mycc -c '{"Args":["set", "a", "10"]}' -C channel12
-
 sleep 5
 
-echo 'Querying For Result on Org1 Peer'
+echo 'Querying For Result on  Peer0'
 
 sudo docker exec -it cli peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
-
-sudo docker exec -it cli peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C channel12
-
-echo 'Invoking Chaincode From Org2 Peer'
+echo 'Invoking Chaincode From  Peer1'
 
 sudo docker exec -it cli2 peer chaincode invoke -o orderer.example.com:7050 -n mycc -c '{"Args":["set", "a", "40"]}' -C mychannel
-
-sudo docker exec -it cli2 peer chaincode invoke -o orderer.example.com:7050 -n mycc -c '{"Args":["set", "a", "30"]}' -C channel12
-
 sleep 5
 
-echo 'Querying For Result on Org2 Peer'
+echo 'Querying For Result on Peer1'
 
 sudo docker exec -it cli2 peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
 
-sudo docker exec -it cli2 peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C channel12
-
-echo 'Invoking Chaincode From Org3 Peer'
+echo 'Invoking Chaincode From  Peer2'
 
 sudo docker exec -it cli3 peer chaincode invoke -o orderer.example.com:7050 -n mycc -c '{"Args":["set", "a", "60"]}' -C mychannel
 
 sleep 5
 
-echo 'Querying For Result on Org3 Peer'
+echo 'Querying For Result on Org3 Peer2'
 
 sudo docker exec -it cli3 peer chaincode query -n mycc -c '{"Args":["query","a"]}' -C mychannel
 
