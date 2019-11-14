a152b01db46a2ae25966eb675f2aaa03df03600c
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-11-14 11:24:54 +0530

    Update backup.sh

--- backup.sh
+++ backup.sh
@@ -9,15 +9,15 @@ cp -r channel-artifacts backup
 
 cd backup
 mkdir peer0.org1
-mkdir peer0.org2
-mkdir peer0.org3
+mkdir peer1.org1
+mkdir peer2.org1
 mkdir orderer
 cd ..
 #Copying Peer and orderer data
 sleep 5
 sudo docker cp peer0.org1.example.com:/var/hyperledger/production/ backup/peer0.org1/
-sudo docker cp peer0.org2.example.com:/var/hyperledger/production/ backup/peer0.org2/
-sudo docker cp peer0.org3.example.com:/var/hyperledger/production/ backup/peer0.org3/
+sudo docker cp peer1.org1.example.com:/var/hyperledger/production/ backup/peer1.org1/
+sudo docker cp peer2.org1.example.com:/var/hyperledger/production/ backup/peer2.org1/
 
 sudo docker cp orderer.example.com:/var/hyperledger/production/orderer/ backup/orderer/
 
