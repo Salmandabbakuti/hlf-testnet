b3c2ba85817be54d4e731c425cb26f97b0fa4aca
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-11-14 11:41:24 +0530

    Update prereqs.sh

--- client/prereqs.sh
+++ client/prereqs.sh
@@ -17,14 +17,7 @@ node enrollAdmin.js
 echo 'Registering User..'
 node registerUser.js
 
-echo 'Enrolling Admin For Org2...'
-
-node enrollAdmin2.js
-
-echo 'Registering User..'
-node registerUser2.js
-
 echo 'All Done..Start Querying or Invoking Calls with node *query.js* and *node invoke.js*'
-echo 'You can Also Use API server for Querying or invoking Calls .. Run *node apiServer.js* and then Point to Serving Ports For your Org..'
+echo 'You can Also Use API server for Querying or invoking Calls .. Run *node server.js* and then Point to 8080 For your Org..'
 
 exit 1
