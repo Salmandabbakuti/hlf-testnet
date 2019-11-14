55c2fe2055e6e110b095514fc6a06d176c938be0
Salman Dabbakuti <dabbakutisalman@gmail.com>
2019-11-14 10:16:10 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -6,7 +6,7 @@ sudo docker network prune
 sudo docker-compose -f docker-compose-cli.yaml up -d
 
 sleep 20
-echo 'Channel Creation Taking Place..'
+echo 'Channel Creation Taking Place by Peer0..'
 
 sudo docker exec -it cli peer channel create -o orderer.example.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -c mychannel -f ./channel-artifacts/channel.tx
 
@@ -19,30 +19,16 @@ sudo docker cp cli:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.
 sudo docker cp mychannel.block cli2:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
 sudo docker cp mychannel.block cli3:/opt/gopath/src/github.com/hyperledger/fabric/peer/mychannel.block
 rm mychannel.block
-echo 'Org2 Peer joining Channel..'
+echo ' Peer1 joining Channel..'
 
 
 sudo docker exec -it cli2 peer channel join -b mychannel.block
 
-echo 'Org3 Peer joining channel...'
+echo 'Peer2 joining channel...'
 
 sudo docker exec -it cli3 peer channel join -b mychannel.block
 
 
 echo 'All Peers Joined mychannel..'
 
-echo 'creating channel12..'
-sudo docker exec -it cli peer channel create -o orderer.example.com:7050 /opt/gopath/src/github.com/hyperledger/fabric/peer/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem -c channel12 -f ./channel-artifacts/channel12.tx
-sudo docker exec -it cli peer channel join -b channel12.block
-
-echo 'exporting channel12.block to org2 peer..'
-
-sudo docker cp cli:/opt/gopath/src/github.com/hyperledger/fabric/peer/channel12.block .
-
-sudo docker cp channel12.block cli2:/opt/gopath/src/github.com/hyperledger/fabric/peer/channel12.block
-
-sudo docker exec -it cli2 peer channel join -b channel12.block
-rm channel12.block
-
-echo 'Peer2 joined channel12 successfully..'
 exit 1
